81bdac01eb4912ef8527d5fc51b76e6463837652
Amyr <98671539+AmyrBaqer@users.noreply.github.com>
2025-04-12 16:37:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -24,4 +25,5 @@ def wallet():
     return jsonify(wallet)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
